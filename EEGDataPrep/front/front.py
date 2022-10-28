7f7ab682993b147b0e47c173fa69697e5a9fcd75
itayinbarr <itayinbar.me@gmail.com>
2022-10-28 11:12:45 +0300

    Arranged UI

--- EEGDataPrep/front/front.py
+++ EEGDataPrep/front/front.py
@@ -1,60 +1,41 @@
 from tkinter import *
-from EEGDataPrep.dataprep.dataprep import hello
-from tkinter import filedialog as fd
-from tkinter.messagebox import showinfo
+from tkinter.filedialog import askopenfilename
+from tkinter import messagebox
 
 
 class MyWindow:
     def __init__(self, win):
-        self.lbl1=Label(win, text='EEG Raw data Processor')
-        self.lbl2=Label(win, text='Create plot of Data')
-        self.lbl3=Label(win, text='Create processed data')
-        self.t1 = Button(win, text='Open a File', command=self.select_file)
-        self.t2=Entry()
-        self.t3=Entry()
-        self.btn1 = Button(win, text='Create plot')
-        self.btn2=Button(win, text='Create file')
-        self.lbl1.place(x=100, y=70)
-        self.t1.place(x=200, y=50)
-        self.lbl2.place(x=100, y=100)
-        self.t2.place(x=200, y=100)
-        self.b1=Button(win, text='Add', command=self.add)
-        self.b2=Button(win, text='Subtract')
-        self.b2.bind('<Button-1>', self.sub)
-        self.b1.place(x=100, y=150)
-        self.b2.place(x=200, y=150)
-        self.lbl3.place(x=100, y=200)
-        self.t3.place(x=200, y=200)
+        # Creating the visuals
+        # Labels
+        self.lblTitle = Label(win, font="Helvetica 16 bold", text='EEG Raw data Processor')
+        self.lblSubtitle = Label(win, text='Choose your EEG data')
+        self.lblBtnLeft = Label(win, font="Helvetica 12 bold", text='Create plot from file')
+        self.lblBtnRight = Label(win, font="Helvetica 12 bold", text='Process Your File')
+        # Buttons
+        self.btnOpenFile = Button(win, text='Load File', command=self.select_file)
+        self.btnLeft = Button(win, text='Create plot', command=self.add)
+        self.btnRight = Button(win, text='Process file', command=self.add)
+        # EEG file loaded by user
+        self.selectedFile = "No file loaded"
+        # Placing the objects
+        # Labels
+        self.lblTitle.place(x=100, y=70)
+        self.lblSubtitle.place(x=100, y=110)
+        self.lblBtnLeft.place(x=100, y=180)
+        self.lblBtnRight.place(x=300, y=180)
+        # Buttons
+        self.btnOpenFile.place(x=100, y=140)
+        self.btnLeft.place(x=100, y=220)
+        self.btnRight.place(x=300, y=220)
 
     def select_file(self):
         filetypes = (
             ('text files', '*.txt'),
             ('All files', '*.*')
         )
-
-        filename = fd.askopenfilename(
-            title='Open a file',
-            initialdir='/',
-            filetypes=filetypes)
-
-        showinfo(
-            title='Selected File',
-            message=filename
-        )
+        self.selectedFile = askopenfilename(filetypes=filetypes, title='Open a file')
+        if not self.selectedFile:
+            messagebox.showerror("Bad File Loaded", "Bad File. Load Again")
 
     def add(self):
-        self.t3.delete(0, 'end')
-        num1=int(self.t1.get())
-        num2=int(self.t2.get())
-        result=num1+num2
-        self.t3.insert(END, str(result))
-
-    def sub(self, event):
-        self.t3.delete(0, 'end')
-        num1=int(self.t1.get())
-        num2=int(self.t2.get())
-        result=num1-num2
-        self.t3.insert(END, str(result))
-
-
-
+        messagebox.showinfo("Selected File", self.selectedFile)
