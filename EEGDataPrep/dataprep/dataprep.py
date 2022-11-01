edfbd6dd4e7d3df7f621678b2112972577e03b84
itayinbarr <itayinbar.me@gmail.com>
2022-11-01 13:03:00 +0200

    Finished functionality

--- EEGDataPrep/dataprep/dataprep.py
+++ EEGDataPrep/dataprep/dataprep.py
@@ -1,59 +1,36 @@
+import os
+
 import mne
+from mne.preprocessing import ICA, create_eog_epochs, create_ecg_epochs
 
-
-# Plot Function
+# Plotting functions
+# ---------------------------
+# Plot raw data
 def plot_raw(file):
     raw = mne.io.read_raw(file, preload=True)
     # Deleting extra channels for development sake. DELETE AFTER FINISHED
-    raw.pick(["Fz", "Pz", "C3", "C4", "Cz"])
+    raw.pick(['Pz', 'Cz', 'Fz', 'C3', 'C4'])
     raw.plot(duration=4)
 
 
+# Plot processed data
+def plot_processed_file(file):
+    data = mne.read_epochs(file, preload=True)
+    data.plot()
+
+# Step One functions
+# ---------------------------
 # Bandpass, high pass, low pass, re-referencing, picking bad channels
 def step1(file):
     raw = mne.io.read_raw(file, preload=True)
     # Deleting extra channels for development sake. DELETE AFTER FINISHED
-    raw.pick(["Fz", "Pz", "C3", "C4", "Cz"])
+    raw.pick(['Pz', 'Cz', 'Fz', 'C3', 'C4'])
     bandpass_filter(raw)
     rereference(raw)
     print("---------------------------------")
     print("Pick bad channels")
     raw.plot(duration=4)
     return raw
-
-
-# Deleting bad channels picked, Epochs
-def step2(raw, file):
-    inspect_bads(raw)
-    epochs = epoching(raw)
-    processedFileName = f"Processed - {file.rsplit('/', 1)[-1].split('.')[0]}.fif"
-    # raw.save(processedFileName)
-
-
-# ICA and save processed file
-def step3(raw, file):
-    processedFileName = f"Processed - {file.rsplit('/', 1)[-1].split('.')[0]}.fif"
-    # raw.save(processedFileName)
-
-
-# Inspecting bad channels after first filtering phase
-def inspect_bads(raw):
-    print("---------------------------------")
-    if len(raw.info['bads']) > 0:
-        raw.pick(picks='eeg', exclude="bads")
-        print("Removed bad channels picked in step one")
-    else:
-        print("No bad channels picked")
-
-
-# Ephoching function
-def epoching(raw):
-    events = mne.make_fixed_length_events(raw, start=5, duration=2.5)
-    reject_criteria = dict(mag=4000e-15,  # 4000 fT
-                           grad=4000e-13,  # 4000 fT/cm
-                           eeg=150e-6,  # 150 µV
-                           eog=250e-6)  # 250 µV
-    return mne.Epochs(raw, events, tmin=-0.2, tmax=0.5, reject=reject_criteria, preload=True)
 
 
 # Bandpass filter function
@@ -70,3 +47,53 @@ def bandpass_filter(raw):
 # Re-referenceing function
 def rereference(raw):
     raw.set_eeg_reference(ref_channels=['Cz'])
+
+
+# Step One functions
+# ---------------------------
+# Deleting bad channels picked, Epochs
+def step2(raw, file):
+    inspect_bads(raw)
+    ica_analysis(raw)
+    epochs = epoching(raw, True)
+    print("---------------------------------")
+    print("Pick bad epochs")
+    epochs.plot()
+    return epochs
+
+
+# Inspecting bad channels after first filtering phase
+def inspect_bads(raw):
+    print("---------------------------------")
+    if len(raw.info['bads']) > 0:
+        raw.pick(picks='eeg', exclude="bads")
+        print("Removed bad channels picked in step one")
+    else:
+        print("No bad channels picked")
+
+
+# Ephoching & inspect ephochs function
+def epoching(raw, reject):
+    events = mne.make_fixed_length_events(raw, start=5, duration=2.5)
+    if reject:
+        reject_criteria = dict(eeg=150e-6)  # 250 µV
+        return mne.Epochs(raw, events, reject=reject_criteria, tmin=-0.2, tmax=0.5, preload=True)
+    else:
+        return mne.Epochs(raw, events, tmin=-0.2, tmax=0.5, preload=True)
+
+
+# ICA analysis
+def ica_analysis(raw):
+    ica = ICA(method='fastica', max_iter='auto')
+    ica.fit(raw)
+    return ica.apply(raw)
+
+
+# Save functions
+# ---------------------------
+# Saving Epochs
+def save_processed_epochs(epochs, file):
+    processed_file_name = f"Processed - {file.rsplit('/', 1)[-1].split('.')[0]}-epo.fif"
+    epochs.save(fname=processed_file_name, overwrite=True)
+    os.rename(processed_file_name, f"../data/preprocessed/{processed_file_name}")
+
--- EEGDataPrep/front/front.py
+++ EEGDataPrep/front/front.py
@@ -13,16 +13,20 @@ class MyWindow:
         self.lblBtnLeft = Label(win, font="Helvetica 14 bold", text='Create plots from file')
         self.lblBtnRight = Label(win, font="Helvetica 14 bold", text='Process Your File')
         self.lblStep1 = Label(win, text='Bandpass, re-referencing & Channel inspection')
-        self.lblStep2 = Label(win, text='Removing bad channels, epoching & Inspecting epochs')
+        self.lblStep2 = Label(win, text='Removing bad channels, epoching, inspecting epochs,')
         self.lblStep3 = Label(win, text='ICA & Save processed file')
         # Buttons
         self.btnOpenFile = Button(win, text='Load File', command=self.select_file)
-        self.btnRawPlot = Button(win, text='Create plot', command=self.create_plot)
+        self.btnRawPlot = Button(win, text='Plot Raw', command=self.create_plot)
+        self.btnProcessedPlot = Button(win, text='Plot Processed', command=self.plot_processed)
         self.btnStep1 = Button(win, text='Step One', command=self.first_step)
         self.btnStep2 = Button(win, text='Step Two', command=self.second_step)
+        self.btnStep3 = Button(win, text='Save Epochs', command=self.save_epochs)
         # EEG file loaded by user
         self.selectedFile = "No file loaded"
         self.midProcess = mne.create_info(4, sfreq=40)
+        self.processed_epochs = mne.create_info(4, sfreq=40)
+        self.savedFileName = "Untitled"
         # Placing the objects
         # Labels
         self.lblTitle.place(x=20, y=50)
@@ -31,17 +35,41 @@ class MyWindow:
         self.lblBtnRight.place(x=190, y=140)
         self.lblStep1.place(x=250, y=180)
         self.lblStep2.place(x=250, y=220)
-        self.lblStep3.place(x=250, y=260)
+        self.lblStep3.place(x=250, y=240)
         # Buttons
         self.btnOpenFile.place(x=170, y=90)
         self.btnRawPlot.place(x=20, y=180)
+        self.btnProcessedPlot.place(x=20, y=220)
         self.btnStep1.place(x=190, y=180)
         self.btnStep2.place(x=190, y=220)
+        self.btnStep3.place(x=190, y=280)
 
+    # Triggered when Load File is pressed
     def select_file(self):
         filetypes = (
             ('set files', '*.set'),
-            ('All files', '*.*')
+            ('BrainVision files', '*.vhdr'),
+            ('BrainVision files', '*.vmrk'),
+            ('BrainVision files', '*.eeg'),
+            ('European data format files', '*.edf'),
+            ('BioSemi data format files', '*.bdf'),
+            ('General data format files', '*.gdf'),
+            ('Neuroscan CNT files', '*.cnt'),
+            ('EGI simple binary files', '*.egi'),
+            ('EGI MFF files', '*.mff'),
+            ('EEGLAB files', '*.set'),
+            ('EEGLAB files', '*.fdt'),
+            ('Nicolet files', '*.data'),
+            ('eXimia EEG data files', '*.nxe'),
+            ('Persyst EEG data files', '*.lay'),
+            ('Persyst EEG data files', '*.dat'),
+            ('Nihon Kohden EEG data files', '*.21e'),
+            ('Nihon Kohden EEG data files', '*.pnt'),
+            ('Nihon Kohden EEG data files', '*.log'),
+            ('Nihon Kohden EEG data files', '*.21e'),
+            ('XDF EEG data files', '*.xdf'),
+            ('XDF EEG data files', '*.xdfz'),
+            ('Elekta NeuroMag data files', '*.fif')
         )
         self.selectedFile = askopenfilename(filetypes=filetypes, title='Open a file')
         if not self.selectedFile:
@@ -50,12 +78,26 @@ class MyWindow:
             print("---------------------------------")
             print(f"{self.selectedFile.rsplit('/', 1)[-1].split('.')[0]}", "selected successfully, click "
                                                                            "Step One to proceed with processing")
+            print(self.selectedFile)
 
+    # Triggered when Plot Raw is pressed
     def create_plot(self):
         plot_raw(self.selectedFile)
 
+    # Plot an already-processed file
+    def plot_processed(self):
+        plot_processed_file(self.selectedFile)
+
+    # Triggered when Step One is pressed
     def first_step(self):
         self.midProcess = step1(self.selectedFile)
 
+    # Triggered when Step Two is pressed
     def second_step(self):
-        step2(self.midProcess, self.selectedFile)
+        self.midProcess = step2(self.midProcess, self.selectedFile)
+
+    # Triggered when Load File is pressed
+    def save_epochs(self):
+        save_processed_epochs(self.midProcess, self.selectedFile)
+        print("---------------------------------")
+        print("Your preprocessed file is new in data/preprocessed folder.")
